b072a202650f6fc27db63ac1e97a574d63576d8e
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-15 11:03:10 +0100

    create funcs to get & set a reflect.Value instance

--- mashaler_funcs.go
+++ mashaler_funcs.go
@@ -1,7 +1,14 @@
 package marshaler
 
 import (
+	"fmt"
 	"reflect"
+	"strconv"
+)
+
+const (
+	csvStructTag        = string("csv")
+	columnNamesPosition = int(0)
 )
 
 func isSlice(v any) bool {
@@ -22,4 +29,56 @@ func isSliceOfStruct(v any) bool {
 
 func isPointerToSliceOfStructs(v any) bool {
 	return isPointer(v) && isSliceOfStruct(reflect.ValueOf(v).Elem().Interface())
+}
+
+//
+
+func getValue(f reflect.Value) (string, error) {
+	switch f.Kind() {
+	case reflect.Int:
+		return strconv.FormatInt(f.Int(), 10), nil
+
+	case reflect.Bool:
+		return strconv.FormatBool(f.Bool()), nil
+
+	case reflect.String:
+		return f.String(), nil
+
+	default:
+		return "", fmt.Errorf("cannot handle value of kind %v", f.Kind())
+	}
+}
+
+func setValue(v reflect.Value, d string) error {
+	switch v.Kind() {
+	case reflect.Int:
+		i, err := strconv.ParseInt(d, 10, 64)
+
+		if err != nil {
+			return err
+		}
+
+		v.SetInt(i)
+
+		return nil
+
+	case reflect.Bool:
+		b, err := strconv.ParseBool(d)
+
+		if err != nil {
+			return err
+		}
+
+		v.SetBool(b)
+
+		return nil
+
+	case reflect.String:
+		v.SetString(d)
+
+		return nil
+
+	default:
+		return fmt.Errorf("cannot handle value of kind %v", v.Kind())
+	}
 }
