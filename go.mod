2143c21767ba61f7a94e39e54f4149485da270a3
Huboh <72440222+huboh@users.noreply.github.com>
2022-07-16 10:27:45 +0100

    REFACTOR: rename package name, module path & files

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/huboh/marshaler
+module github.com/huboh/records
 
 go 1.18
 
--- records.go
+++ records.go
@@ -1,2 +1,3 @@
 // Package marshaler provides utility functions to marshals & unmarshal parsed CSV rows of data to or from a slice of slice of strings to the fields in a struct.
-package marshaler
+package records
+
